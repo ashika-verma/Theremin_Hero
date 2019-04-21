d98a75672ceb3a67c542682203f3ae31fdf45f6f
obada <obada@mit.edu>
2019-04-20 20:44:55 -0400

    handled uploaded songs

--- server/server.py
+++ server/server.py
@@ -1,2 +1,20 @@
+import sqlite3
+import datetime
+song_db = "__HOME__/songs.db"
+
+def handle_post(request):
+	conn = sqlite3.connect(song_db)
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS song_table (song_name text,song text, timing timestamp);''')
+
+	songname = request['values']['name']
+	song = request['values']['song']
+
+	c.execute('''INSERT into song_table VALUES (?,?,?);''', (songname,song,datetime.datetime.now()))
+
+	return "Song uploaded!"
+
 def request_handler(request):
-  return request
+	if request['method'] == 'POST':
+		return handle_post(request)
+  	return request
