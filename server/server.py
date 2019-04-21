c41f3c54a04958d229e5cab1bb2baa92a349ef27
Kendall Garner <17521368+kgarner7@users.noreply.github.com>
2019-04-20 20:51:22 -0400

    slghtly better html and indentation fixed?

--- server/server.py
+++ server/server.py
@@ -4,19 +4,19 @@ song_db = "__HOME__/songs.db"
 
 def handle_post(request):
 	conn = sqlite3.connect(song_db)
-    c = conn.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS song_table (song_name text,song text, timing timestamp);''')
+	c = conn.cursor()
+	c.execute('''CREATE TABLE IF NOT EXISTS song_table (song_name text,song text, timing timestamp);''')
 
-	songname = request['values']['name']
-	song = request['values']['song']
+	songname = request['form']['songName']
+	song = request['form']['musicString']
 
 	c.execute('''INSERT into song_table VALUES (?,?,?);''', (songname,song,datetime.datetime.now()))
 	conn.commit()
-    conn.close()
+	conn.close()
 
 	return "Song uploaded!"
 
 def request_handler(request):
 	if request['method'] == 'POST':
 		return handle_post(request)
-  	return request
+	return request
