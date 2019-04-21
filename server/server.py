ed8c859093af04f135138f46f3667db7bc6b3edb
obada <obada@mit.edu>
2019-04-20 20:48:26 -0400

    fixed indentation

--- server/server.py
+++ server/server.py
@@ -3,20 +3,20 @@ import datetime
 song_db = "__HOME__/songs.db"
 
 def handle_post(request):
-	conn = sqlite3.connect(song_db)
+    conn = sqlite3.connect(song_db)
     c = conn.cursor()
     c.execute('''CREATE TABLE IF NOT EXISTS song_table (song_name text,song text, timing timestamp);''')
 
-	songname = request['values']['name']
-	song = request['values']['song']
+    songname = request['values']['name']
+    song = request['values']['song']
 
-	c.execute('''INSERT into song_table VALUES (?,?,?);''', (songname,song,datetime.datetime.now()))
-	conn.commit()
+    c.execute('''INSERT into song_table VALUES (?,?,?);''', (songname,song,datetime.datetime.now()))
+    conn.commit()
     conn.close()
 
-	return "Song uploaded!"
+    return "Song uploaded!"
 
 def request_handler(request):
-	if request['method'] == 'POST':
-		return handle_post(request)
-  	return request
+    if request['method'] == 'POST':
+        return handle_post(request)
+    return request
