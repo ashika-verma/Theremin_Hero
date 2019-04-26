d0b1af0a139f7bd99fcb6ae29aa361ca65d98b59
obada <obada@mit.edu>
2019-04-26 19:09:38 -0400

    added handling for GET requests

--- server/server.py
+++ server/server.py
@@ -2,26 +2,55 @@ import sqlite3
 import datetime
 song_db = "__HOME__/songs.db"
 
+
 def handle_post(request):
 	conn = sqlite3.connect(song_db)
 	c = conn.cursor()
-	c.execute('''CREATE TABLE IF NOT EXISTS song_table (song_name text,song text, timing timestamp);''')
+	c.execute('''CREATE TABLE IF NOT EXISTS song_table (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+														songName TEXT, 
+														song TEXT, 
+														timing TIMESTAMP);''')
 
-	songname = request['form']['songName']
+	songName = request['form']['songName']
 	song = request['form']['musicString']
 
-	c.execute('''INSERT into song_table VALUES (?,?,?);''', (songname,song,datetime.datetime.now()))
+	c.execute('''INSERT OR IGNORE into song_table VALUES (?,?,?);''', (songName,song,datetime.datetime.now()))
 
-	db_stuff = c.execute('''SELECT * FROM song_table''').fetchall()
+	dbSongs = c.execute('''SELECT * FROM song_table''').fetchall()
 	outs = ''
-	for thing in db_stuff:
-		outs += 'song name: ' + thing[0] + ', song: ' + thing[1] + ', timestamp: ' + thing[2] + '\n'
+	for thing in dbSongs:
+		outs += 'song id: ' + thing[0] + ', song name: ' + thing[1] + ', song: ' + thing[2] + ', timestamp: ' + thing[3] + '\n'
+
 	conn.commit()
 	conn.close()
 
 	return outs
 
+
+def handle_get(request):
+	conn = sqlite3.connect(song_db)
+	c = conn.cursor()
+	c.execute('''CREATE TABLE IF NOT EXISTS song_table (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+														songName TEXT UNIQUE, 
+														song TEXT, 
+														timing TIMESTAMP);''')
+
+	if "songName" in request["args"]:
+		dbSongs = c.execute('''SELECT * FROM song_table WHERE songName = ? ORDER BY timing''', (songName)).fetchall()
+	else:
+		dbSongs = c.execute('''SELECT * FROM song_table ORDER BY timing ASC''').fetchall()
+	songs = ''
+	for song in dbSongs:
+		songs += song[0] + ',' + song[2] + '\n'
+
+	conn.commit()
+	conn.close()
+
+	return songs
+
+
 def request_handler(request):
 	if request['method'] == 'POST':
 		return handle_post(request)
-	return request
+	else:
+		return handle_get(request)
