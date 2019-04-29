ca30bc23e192c1c10c3853b4aff462aa1aab670b
Kendall Garner <>
2019-04-29 18:38:00 -0400

    more server changes song upload

--- server/html/scripts.js
+++ server/html/scripts.js
@@ -1,18 +1,29 @@
 $(function() {
+  function showSong(name, html) {
+    $("#song-name").text(name);
+    $("#song-tab").show().click().text(name);
+    $("#song-content").html(html);
+  }
+
   $("#logo").on("click", function(evt) {
     $("#home-tab").click();
   });
 
   $("#songs-body").on("click", "a", function(e) {
-    $("#song-tab").click();
-    var id = $(e.target).data("id");
+    var target = $(e.target);
+    var id = target.data("id");
+    var name = target.text();
     $.get("https://608dev.net/sandbox/sc/kgarner/project/server.py?id=" + id)
       .done(function(result) {
-        $("#song-content").html(result);
+        showSong(name, result);
       });
   });
 
   $("#windowTabs a.nav-link").on("shown.bs.tab", function(e) {
+    if (e.target.id !== "song-tab") {
+      $("#song-tab").hide();
+    }
+
     if (e.target.id === "songs-tab") {
       $.get("https://608dev.net/sandbox/sc/kgarner/project/server.py")
         .done(function(result) {
@@ -35,8 +46,7 @@ $(function() {
 
     $.post("https://608dev.net/sandbox/sc/kgarner/project/server.py", data)
       .done(function(result) {
-        $("#song-tab").click();
-        $("#song-content").html(result);
+        showSong(data["songName"], result);
       }).fail(function(err) {
         alert("Error: " + err);
       });
