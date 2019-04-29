1a93f081fa746097ed86930c3a5cb60abdcacf3f
obada <obada@mit.edu>
2019-04-29 19:34:35 -0400

    fixed freq_est

--- server/freq_est.py
+++ server/freq_est.py
@@ -1,8 +1,32 @@
 import numpy as np
 import soundfile as sf
+from numpy import polyfit, arange
 from scipy.signal import blackmanharris, fftconvolve
 
 # from: https://gist.github.com/endolith/255291
+
+def parabolic(f, x):
+    """Quadratic interpolation for estimating the true position of an
+    inter-sample maximum when nearby samples are known.
+   
+    f is a vector and x is an index for that vector.
+   
+    Returns (vx, vy), the coordinates of the vertex of a parabola that goes
+    through point x and its two neighbors.
+   
+    Example:
+    Defining a vector f with a local maximum at index 3 (= 6), find local
+    maximum if points 2, 3, and 4 actually defined a parabola.
+   
+    In [3]: f = [2, 3, 1, 6, 4, 2, 3, 1]
+   
+    In [4]: parabolic(f, argmax(f))
+    Out[4]: (3.2142857142857144, 6.1607142857142856)
+   
+    """
+    xv = 1/2. * (f[x-1] - f[x+1]) / (f[x-1] - 2 * f[x] + f[x+1]) + x
+    yv = f[x] - 1/4. * (f[x-1] - f[x+1]) * (xv - x)
+    return (xv, yv)
 
 def freq_from_fft(sig, fs):
     """
@@ -13,7 +37,7 @@ def freq_from_fft(sig, fs):
     f = np.rfft(windowed)
 
     # Find the peak and interpolate to get a more accurate peak
-    i = argmax(abs(f))  # Just use this for less-accurate, naive version
+    i = np.argmax(abs(f))  # Just use this for less-accurate, naive version
     true_i = parabolic(log(abs(f)), i)[0]
 
     # Convert to equivalent frequency
@@ -22,4 +46,3 @@ def freq_from_fft(sig, fs):
 
 def to_esp_encoding(sig, fs):
     return "".join([str(freq_from_fft(sub_sig, fs)) + "," + "1;" for sub_sig in np.split(sig, len(sig)/(fs*0.1))])
-    
