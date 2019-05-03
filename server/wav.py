ece2f82ce1bce9c24ba01e05fba788776f615200
obada <obada@mit.edu>
2019-05-03 19:47:23 -0400

    another attempt at smoothing

--- server/wav.py
+++ server/wav.py
@@ -8,24 +8,29 @@ fs = 44100       # sampling rate, Hz, must be integer
 block_length = 1 # in seconds, may be float
 
 
-def create_sample(frequency: float, start: int, end: int):
+def create_sample(frequency: float, start: float, end: float):
   return np.sin(2 * np.pi * np.arange(start, end) * frequency / fs)
 
 
 def create_song(notes: list):
   song = []
-  start = 0
+  last_angle, start = 0.0, 0.0
 
   for note in notes:
+
     freq, dur = note.split(",")
 
     freq = float(freq)
     dur = int(dur)
-    end = start + dur * fs
 
-    song.append(create_sample(freq, start, end))
+    start = (fs * last_angle) / (2 * np.pi * freq) + 1
+    end = start + block_length * fs * dur
+    sample = create_sample(freq, start, end)
 
-    start = end
+    song.append(sample)
+
+    sin1, sin2 = sample[-1], sample[-2]
+    last_angle = np.arcsin(sin1) if sin1 >= sin2 else np.pi - np.arcsin(sin1)
 
   return np.concatenate(song)
 
@@ -37,4 +42,5 @@ def write_song_from_string(name: str, song: str):
 
 
 if __name__ == "__main__":
-	pass
+  # write_song_from_string("song", "290,1;310,1;330,1;350,1;370,1;390,1;410,1")
+  pass
