59850af1213194abb5e59a05267f6c65869d4848
Kendall Garner <>
2019-05-10 19:49:20 -0400

    updated ui for showing scores

--- server/html/scripts.js
+++ server/html/scripts.js
@@ -1,5 +1,7 @@
 $(function() {
-  var songId, userId, showScores = true;
+  $("#song-tab").hide();
+
+  var songId, songName, showScores = true;
 
   function showSong(name, html) {
     $("#song-name").text(name);
@@ -14,7 +16,9 @@ $(function() {
   $("#songs-body").on("click", "a", function(e) {
     var target = $(e.target);
     var id = target.data("id");
-    var name = target.text();
+    songName = target.text();
+    var name = songName;
+
     $.get("https://608dev.net/sandbox/sc/kgarner/project/server.py?id=" + id)
       .done(function(result) {
         songId = id;
@@ -25,7 +29,12 @@ $(function() {
   $("#windowTabs a.nav-link").on("shown.bs.tab", function(e) {
     if (e.target.id !== "song-tab") {
       $("#song-tab").hide();
+      $("#score-button").text("Show scores");
+      $("#scores").html("").hide();
+
       songId = null;
+      songName = null;
+      showScores = true;
     }
 
     if (e.target.id === "songs-tab") {
@@ -42,29 +51,67 @@ $(function() {
     }
   });
 
+  function getAllScores() {
+    $.get("https://608dev.net/sandbox/sc/kgarner/project/score_server.py?songId=" + songId)
+    .done(function(result) {
+      var html;
+      if (result.startsWith("NO RESULTS FOUND")) {
+        html = `<div>No scores found for ${songName}</div>`;
+      } else {
+        html = $.map(result.split("\n"), function(line) {
+          var content = line.split(",");
+          return `<div><a class="score" href="#" data-user="${content[0]}">${content[0]}</a>: ${content[1]} at ${content[2]}</div>`;
+        });
+      }
+
+      showScores = false;
+
+      $("#score-button").text("Hide scores");
+      $("#scores").html(html).show();
+      $("#user-button").hide();
+    }).fail(function(err) {
+      alert("Error: " + err);
+    });
+  }
+
   $("#score-button").on("click", function(evt) {
     if (showScores) {
-      $.get("https://608dev.net/sandbox/sc/kgarner/project/score_server.py?songId=" + songId)
-        .done(function(result) {
-          if (result.startsWith("NO RESULTS FOUND")) {
-            alert("Error: no song with id " + songId + " found");
-          }
-
-          console.log(result);
-        }).fail(function(err) {
-          alert("Error: " + err);
-        });
-      $(this).text("Hide scores");
-      $("#scores").html(result).show();
+      console.log("getting scores");
+      getAllScores();
     } else {
-      showScores = false;
-      $(this).text("Show scores");
+      showScores = true;
+      $("#score-button").text("Show scores");
       $("#scores").html("").hide();
+      $("#user-button").hide();
     }
   });
 
+  $("#user-button").on("click", function(evt) {
+    $(this).hide();
+    getAllScores();
+  });
 
-  $("#song-tab").hide();
+  $("#scores").on("click", "a", function(evt) {
+    var target = $(evt.target);
+    var user = target.data("user");
+
+    $.get(`https://608dev.net/sandbox/sc/kgarner/project/score_server.py?songId=${songId}&&userName=${user}`).done(function(result) {
+      var html = "";
+      if (result.startsWith("NO RESULTS FOUND")) {
+        html = `<div>No scores found for ${songName}</div>`;
+      } else {
+        html = `<div>Score for: ${user}</div>` + $.map(result.split("\n"), function(line) {
+          var content = line.split(",");
+          return `<div>${content[0]} at ${content[1]}</div>`;
+        });
+      }
+
+      $("#scores").html(html).show();
+      $("#user-button").show();
+    }).fail(function(err) {
+      alert("Error: " + err);
+    });
+  });
 
   $("#song-upload").on("submit", function(evt) {
     var data = $("form").serializeArray().reduce(function(total, curr) {
